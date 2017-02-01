fd5a15ba909ecdfea86f71559e96f69df7295ac0
anushkatawakley <anushkatawakley@hotmail.com>
2017-02-01 15:30:20 -0500

    Anushka

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * anushka Theme Customizer
+ *
+ * @package anushka
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function anushka_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'anushka_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function anushka_customize_preview_js() {
+	wp_enqueue_script( 'anushka_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'anushka_customize_preview_js' );
